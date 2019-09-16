da62becb2cd4d84cb9317d4a7044a7c6f65d4f18
Ewgeny <kuzmich-ewgen@list.ru>
2019-09-16 23:29:16 +0300

    change static file

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/photos', express.static(path.join(__dirname, '/photos')));
+app.use('/photos', express.static(path.join(__dirname, '../../photos')));
 
 app.use('/', loginRouter);
 app.use('/login', loginRouter);
