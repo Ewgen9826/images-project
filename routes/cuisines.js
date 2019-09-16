c93e00da83927f1b32ed617656baadb26792a3cf
Ewgeny <kuzmich-ewgen@list.ru>
2019-09-16 21:44:59 +0300

    change 54

--- routes/cuisines.js
+++ routes/cuisines.js
@@ -14,14 +14,12 @@ var adminData = require('../models/login.model');
 const rs = () => Math.random().toString(36).slice(-12);
 //Image storage
 const storage = multer.diskStorage({
-  // destination: (req, file, cb) => {
-  //   const dir = '/' + rs();
-  //   req.dir = dir;
-  //   mkdirp(config.DESTINATION + dir, err => cb(err, config.DESTINATION + dir))
+  destination: (req, file, cb) => {
+    const dir = '/photos/' + rs();
+    req.dir = dir;
+    mkdirp(config.DESTINATION + dir, err => cb(err, config.DESTINATION + dir))
     
-  // },
-  destination: function(req, file, cb) {
-    cb(null, "../"+__dirname + '/photos')  },
+  },
   filename: (req, file, cb) => {
     fileName = 'original' + path.extname(file.originalname);
     cb(null, fileName);
